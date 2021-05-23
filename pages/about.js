cb01b214d6db6ecaa695a1aa18d8407d2bda9ef5
Joonas Sandell <me@joonassandell.com>
2021-05-23 17:37:59 +0300

    Bunch of tweaks

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,48 @@ const home = getSitemap('home');
 export default function About() {
   return (
     <Template name={about.id} title={about.title}>
-      About
+      <h1>H1 heading dolor sit amet consectetur</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nihil
+        labore in nostrum doloribus facilis laudantium provident omnis eos!
+        Molestiae, distinctio eos nihil reprehenderit natus quibusdam repellat
+        sequi incidunt velit.
+      </p>
+      <h2>H2 heading dolor sit amet consectetur</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nihil
+        labore in nostrum doloribus facilis laudantium provident omnis eos!
+        Molestiae, distinctio eos nihil reprehenderit natus quibusdam repellat
+        sequi incidunt velit.
+      </p>
+      <h3>H3 heading dolor sit amet consectetur</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nihil
+        labore in nostrum doloribus facilis laudantium provident omnis eos!
+        Molestiae, distinctio eos nihil reprehenderit natus quibusdam repellat
+        sequi incidunt velit.
+      </p>
+      <h4>H4 heading dolor sit amet consectetur</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nihil
+        labore in nostrum doloribus facilis laudantium provident omnis eos!
+        Molestiae, distinctio eos nihil reprehenderit natus quibusdam repellat
+        sequi incidunt velit.
+      </p>
+      <h5>H5 heading dolor sit amet consectetur</h5>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nihil
+        labore in nostrum doloribus facilis laudantium provident omnis eos!
+        Molestiae, distinctio eos nihil reprehenderit natus quibusdam repellat
+        sequi incidunt velit.
+      </p>
+      <h6>H6 heading</h6>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime nihil
+        labore in nostrum doloribus facilis laudantium provident omnis eos!
+        Molestiae, distinctio eos nihil reprehenderit natus quibusdam repellat
+        sequi incidunt velit.
+      </p>
     </Template>
   );
 }
