cc7ecf5e13722ce6b984e4c52f5818059f81844b
Joonas Sandell <me@joonassandell.com>
2021-06-01 12:25:13 +0300

    Add some lerp to info comp

--- components/Info/Info.js
+++ components/Info/Info.js
@@ -25,7 +25,7 @@ const infoRulerVariants = {
 
 const infoGridVariants = {
   inView: {
-    transition: { staggerChildren: 0.1 },
+    transition: { staggerChildren: 0.15 },
   },
 };
 
@@ -82,82 +82,93 @@ const Info = ({
       animate={inView === 'info' ? 'inView' : false}
       className="Info"
       data-scroll
-      data-scroll-id="info"
       data-scroll-call="info"
-      data-scroll-position="top"
+      data-scroll-id="info"
+      data-scroll-offset="10%"
       initial="hidden"
     >
       <div className="wrap">
-        <motion.hr className="Info-ruler" variants={infoRulerVariants} />
-        <motion.div className="grid" variants={infoGridVariants}>
-          <div className="Info-metaCol grid-col5@l">
-            <div className="grid">
-              <motion.div
-                className="grid-col6 grid-col8@s grid-col9@m grid-col5@l"
-                variants={infoGridCellVariants}
-              >
-                <div className="grid">
-                  <div className="grid-col3@s grid-col4@m grid-col12@l">
-                    <h6>Client</h6>
-                    <p>
-                      <ConditionalWrapper
-                        condition={client.href}
-                        wrapper={children => (
-                          <Link href={client.href}>{children}</Link>
-                        )}
-                      >
-                        {client.name}
-                      </ConditionalWrapper>
-                    </p>
+        <div
+          data-scroll
+          data-scroll-delay="1"
+          data-scroll-position="top"
+          data-scroll-speed="1"
+        >
+          <motion.hr className="Info-ruler" variants={infoRulerVariants} />
+        </div>
+        <div
+          data-scroll
+          data-scroll-position="top"
+          data-scroll-delay="0.05"
+          data-scroll-speed="1"
+        >
+          <motion.div className="grid" variants={infoGridVariants}>
+            <div className="Info-metaCol grid-col5@l">
+              <div className="grid">
+                <motion.div
+                  className="grid-col6 grid-col8@s grid-col9@m grid-col5@l"
+                  variants={infoGridCellVariants}
+                >
+                  <div className="grid">
+                    <div className="grid-col3@s grid-col4@m grid-col12@l">
+                      <h6>Client</h6>
+                      <p>
+                        <ConditionalWrapper
+                          condition={client.href}
+                          wrapper={children => (
+                            <Link href={client.href}>{children}</Link>
+                          )}
+                        >
+                          {client.name}
+                        </ConditionalWrapper>
+                      </p>
+                    </div>
+                    <div className="grid-col3@s grid-col4@m grid-col12@l">
+                      <h6>Year</h6>
+                      <p>{year}</p>
+                    </div>
+                    <div className="grid-col4@s grid-col4@m grid-col12@l">
+                      <h6>Employer</h6>
+                      <p>
+                        <ConditionalWrapper
+                          condition={employer.href}
+                          wrapper={children => (
+                            <Link href={employer.href}>{children}</Link>
+                          )}
+                        >
+                          {employer.name}
+                        </ConditionalWrapper>
+                      </p>
+                    </div>
                   </div>
-                  <div className="grid-col3@s grid-col4@m grid-col12@l">
-                    <h6>Year</h6>
-                    <p>{year}</p>
-                  </div>
-                  <div className="grid-col4@s grid-col4@m grid-col12@l">
-                    <h6>Employer</h6>
-                    <p>
-                      <ConditionalWrapper
-                        condition={employer.href}
-                        wrapper={children => (
-                          <Link href={employer.href}>{children}</Link>
-                        )}
-                      >
-                        {employer.name}
-                      </ConditionalWrapper>
-                    </p>
-                  </div>
-                </div>
-              </motion.div>
-              <motion.div
-                className="Info-roleCol grid-col6 grid-col4@s grid-col3@m grid-col5@l"
-                variants={infoGridCellVariants}
-              >
-                <h6>Role</h6>
-                <ul className="resetList">
-                  {role.map((r, i) => (
-                    <li key={i}>{r}</li>
-                  ))}
-                </ul>
-                <p>
-                  <small className="Text Text--meta">{smallPrint}</small>
-                </p>
-              </motion.div>
+                </motion.div>
+                <motion.div
+                  className="Info-roleCol grid-col6 grid-col4@s grid-col3@m grid-col5@l"
+                  variants={infoGridCellVariants}
+                >
+                  <h6>Role</h6>
+                  <ul className="resetList">
+                    {role.map((r, i) => (
+                      <li key={i}>{r}</li>
+                    ))}
+                  </ul>
+                  <p>
+                    <small className="Text Text--meta">{smallPrint}</small>
+                  </p>
+                </motion.div>
+              </div>
             </div>
-          </div>
-          <motion.div
-            className="Info-headingCol grid-col grid-col9@m grid-col7@l"
-            variants={infoGridCellVariants}
-          >
-            <h2 className="h4">{heading}</h2>
+            <motion.div
+              className="Info-headingCol grid-col grid-col9@m grid-col7@l"
+              variants={infoGridCellVariants}
+            >
+              <h2 className="Info-heading h4">{heading}</h2>
+              <div className="grid">
+                <div className="Info-text grid-col grid-col6@s">{text}</div>
+              </div>
+            </motion.div>
           </motion.div>
-          <motion.div
-            className="Info-textCol grid-col grid-col6@s grid-col4@l -start6@l"
-            variants={infoGridCellVariants}
-          >
-            {text}
-          </motion.div>
-        </motion.div>
+        </div>
       </div>
     </motion.div>
   );
