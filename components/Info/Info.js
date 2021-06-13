e267b306c026f717754ebc9f58393d26154ee86b
Joonas Sandell <me@joonassandell.com>
2021-06-13 23:02:32 +0300

    Tweak info comp

--- components/Info/Info.js
+++ components/Info/Info.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useLocomotiveScroll } from 'react-locomotive-scroll';
 import {
+  scrollSpeed,
   transPrimary,
   transTertiary,
   transPrimaryFast,
@@ -9,6 +10,7 @@ import {
   transSecondaryFast,
   transSecondaryFastest,
 } from '../../lib/config';
+import { isInView } from '../../lib/utility';
 import { motion, useAnimation } from 'framer-motion';
 import Link from '../Link';
 import ConditionalWrapper from '../ConditionalWrapper';
@@ -51,66 +53,51 @@ const Info = ({
   year,
 }) => {
   const { scroll } = useLocomotiveScroll();
-  const [inView, setInView] = useState(null);
-
-  const inViewStart = el => {
-    console.log('call');
-    if (inView != el) {
-      setInView(el);
-    }
-  };
+  const [inView, setInView] = useState(false);
 
   if (scroll) {
-    scroll.on('call', inViewStart);
-
-    // scroll.on('scroll', args => {
-    //   // console.log(args);
-    //   if (typeof args.currentElements['info'] === 'object') {
-    //     setInView(args.currentElements['info'].id);
-    //   }
-    // });
+    scroll.on('scroll', args => isInView(args, 'info', () => setInView(true)));
   }
 
   useEffect(() => {
-    () => {
-      scroll.off('call', inViewStart);
-    };
+    () => scroll.off('call', inViewStart);
   }, [scroll]);
 
   return (
     <motion.div
-      animate={inView === 'info' ? 'inView' : false}
+      animate={inView ? 'inView' : false}
       className="Info"
       data-scroll
-      data-scroll-call="info"
       data-scroll-id="info"
-      data-scroll-offset="10%"
+      // data-scroll-offset="10%"
+      // data-scroll-speed="1"
+      // data-scroll-position="top"
       initial="hidden"
     >
       <div className="wrap">
         <div
           data-scroll
-          data-scroll-delay="1"
+          // data-scroll-delay="1"
           data-scroll-position="top"
-          data-scroll-speed="1"
+          data-scroll-speed="0.5" // 1
         >
           <motion.hr className="Info-ruler" variants={infoRulerVariants} />
         </div>
         <div
-          data-scroll
-          data-scroll-position="top"
-          data-scroll-delay="0.05"
-          data-scroll-speed="1"
+        // data-scroll
+        // data-scroll-delay="0.05"
+        // data-scroll-position="top"
+        // data-scroll-speed={scrollSpeed} // 1
         >
           <motion.div className="grid" variants={infoGridVariants}>
-            <div className="Info-metaCol grid-col5@l">
+            <div className="Info-metaCol grid-col:5@l">
               <div className="grid">
                 <motion.div
-                  className="grid-col6 grid-col8@s grid-col9@m grid-col5@l"
+                  className="grid-col:6 grid-col:8@s grid-col:9@m grid-col:5@l"
                   variants={infoGridCellVariants}
                 >
                   <div className="grid">
-                    <div className="grid-col3@s grid-col4@m grid-col12@l">
+                    <div className="grid-col:3@s grid-col:4@m grid-col:12@l">
                       <h6>Client</h6>
                       <p>
                         <ConditionalWrapper
@@ -123,11 +110,11 @@ const Info = ({
                         </ConditionalWrapper>
                       </p>
                     </div>
-                    <div className="grid-col3@s grid-col4@m grid-col12@l">
+                    <div className="grid-col:3@s grid-col:4@m grid-col:12@l">
                       <h6>Year</h6>
                       <p>{year}</p>
                     </div>
-                    <div className="grid-col4@s grid-col4@m grid-col12@l">
+                    <div className="grid-col:4@s grid-col:4@m grid-col:12@l">
                       <h6>Employer</h6>
                       <p>
                         <ConditionalWrapper
@@ -143,7 +130,7 @@ const Info = ({
                   </div>
                 </motion.div>
                 <motion.div
-                  className="Info-roleCol grid-col6 grid-col4@s grid-col3@m grid-col5@l"
+                  className="Info-roleCol grid-col:6 grid-col:4@s grid-col:3@m grid-col:5@l"
                   variants={infoGridCellVariants}
                 >
                   <h6>Role</h6>
@@ -159,12 +146,12 @@ const Info = ({
               </div>
             </div>
             <motion.div
-              className="Info-headingCol grid-col grid-col9@m grid-col7@l"
+              className="Info-headingCol grid-col:9@m grid-col:7@l"
               variants={infoGridCellVariants}
             >
               <h2 className="Info-heading h4">{heading}</h2>
               <div className="grid">
-                <div className="Info-text grid-col grid-col6@s">{text}</div>
+                <div className="Info-text grid-col:6@s">{text}</div>
               </div>
             </motion.div>
           </motion.div>
