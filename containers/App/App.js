0e19527601b7454f17b1ffe34963a626c42192f1
Joonas Sandell <me@joonassandell.com>
2021-06-14 21:00:08 +0300

    Disable zooming for now because it causes weird behavior

--- containers/App/App.js
+++ containers/App/App.js
@@ -60,7 +60,7 @@ export function App({ children }) {
       <Head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=1, user-scalable=no"
         />
         <script
           dangerouslySetInnerHTML={{
