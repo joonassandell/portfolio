a36daa18a437715c7b4e48f4cc98e0de76298024
Joonas Sandell <me@joonassandell.com>
2023-06-06 04:02:23 +0300

    Footer: inherit template bg color, add neat color filtering

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -7,22 +7,24 @@ export const Footer = () => {
   return (
     <footer className="Footer wrap">
       <div className="Footer-inner">
-        <ul className="Footer-links Footer-links--mobile">
-          {someLinks.map(link => {
-            return (
-              <li className="Footer-links-item" key={link.id}>
-                <LinkRoll href={link.url} underline>
-                  {link.title}
-                </LinkRoll>
-              </li>
-            );
-          })}
-        </ul>
-        <p className="Footer-copyright">
-          &copy; {new Date().getFullYear()}
-          <br />
-          Joonas Sandell
-        </p>
+        <div className="Footer-main">
+          <ul className="Footer-links Footer-links--mobile">
+            {someLinks.map(link => {
+              return (
+                <li className="Footer-links-item" key={link.id}>
+                  <LinkRoll href={link.url} underline>
+                    {link.title}
+                  </LinkRoll>
+                </li>
+              );
+            })}
+          </ul>
+          <p className="Footer-copyright">
+            &copy; {new Date().getFullYear()}
+            <br />
+            Joonas Sandell
+          </p>
+        </div>
       </div>
     </footer>
   );
