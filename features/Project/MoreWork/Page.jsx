6fd9cc93bbd2fdc2e4e81ee9c55282b15ebf23f9
Joonas Sandell <me@joonassandell.com>
2023-06-25 07:20:41 +0300

    MoreWork: Add HW-Company etc.

--- features/Project/MoreWork/Page.jsx
+++ features/Project/MoreWork/Page.jsx
@@ -5,6 +5,7 @@ import { NextProject } from '@/components/NextProject';
 import { MQ } from '@/lib/config';
 import { Info } from '@/components/Info';
 import { Figure } from '@/components/Figure';
+import { Text } from '@/components/Text';
 import { Title } from '@/components/Title';
 
 export const MoreWorkPage = ({ images, id, title, year }) => {
@@ -34,6 +35,55 @@ export const MoreWorkPage = ({ images, id, title, year }) => {
           smallPrint="Some projects may present initial designs and prototypes."
           year={year}
         />
+        <TemplateSection gridRowGap="l" paddingTop="10vw">
+          <SubInfo
+            client={{ name: 'HW-Company' }}
+            heading="HW-Company"
+            role={['UI/UX design', 'Web development']}
+            text={
+              <p>
+                HW-Company Ltd is a wholesale service company that imports and
+                sells internationally known and high-quality accessories, shoes
+                and equipment for outdoor activities.
+              </p>
+            }
+            type={['Web service', 'E-commerce', 'Commission']}
+            year={2018}
+          />
+          <div className="grid-col grid-col:7@m grid-col:6@l">
+            <Figure
+              alt="HW-Company home page animation"
+              scrolling={false}
+              src="/more-work/joonassandell-hw-company-home-animation.mp4"
+            />
+          </div>
+          <div className="grid-col grid-col:8@m">
+            <Figure
+              alt="HW-Company home page"
+              scrolling={false}
+              sizes={`${MQ.m} 70vw, 100vw`}
+              {...getImage('joonassandell-hw-company-home', images)}
+            />
+          </div>
+          <div className="grid-col grid-col:10 grid-col:4@m">
+            <Figure
+              alt="HW-Company products page"
+              scrolling={false}
+              inViewOffset={0.5}
+              sizes={`${MQ.m} 33vw, 80vw`}
+              {...getImage('joonassandell-hw-company-products', images)}
+            />
+            <Text className="mt:xl" tag="p">
+              I was asked to design and develop website for the companys newly
+              updated brand. The main goal was to improve HW-Company's brand,
+              improve usability, and give website users a comprehensive picture
+              of HW-Company's activities around the world.
+            </Text>
+            <Text color="light" tag="small">
+              Built together with awesome people from Porkka & Kuutsa.
+            </Text>
+          </div>
+        </TemplateSection>
         <TemplateSection gridRowGap="l" paddingTop="15vw">
           <SubInfo
             client={{ name: 'Hankkija' }}
@@ -43,10 +93,7 @@ export const MoreWorkPage = ({ images, id, title, year }) => {
               <p>
                 Hankkija Finnish Feed Innovations have their roots in the long
                 tradition of developing novel solutions in Finnish bioscience
-                industry. I had the honor to design and develop their new
-                international website. The fresh user interface innovated the
-                client to expand the design to other marketing materials as
-                well.
+                industry.
               </p>
             }
             type={['Web service', 'Commission']}
@@ -77,13 +124,20 @@ export const MoreWorkPage = ({ images, id, title, year }) => {
               {...getImage('joonassandell-hankkija-home-2', images)}
             />
           </div>
-          <div className="grid-col grid-col:10 -start:3 grid-col:6@m -start:4@m">
+          <div className="grid-col grid-col:10 -start:3 grid-col:6@m">
             <Figure
               alt="Hankkija subpages"
               scrolling={false}
               sizes={`${MQ.m} 33vw, 80vw`}
               {...getImage('joonassandell-hankkija-views', images)}
             />
+          </div>
+          <div className="grid-col grid-col:10@s grid-col:4@m">
+            <Text tag="p">
+              I had the honor to design and develop Hankkija's new international
+              website. The fresh user interface innovated the client to expand
+              the design to other marketing materials as well.
+            </Text>
           </div>
         </TemplateSection>
         <TemplateSection gridRowGap="l" paddingTop="15vw">
