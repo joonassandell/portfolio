6ae61749a1a93dd26c091a209d6b9f0f3d5c8670
Joonas Sandell <me@joonassandell.com>
2023-06-26 05:05:29 +0300

    App: Allow zooming

--- components/App/App.jsx
+++ components/App/App.jsx
@@ -135,7 +135,7 @@ export const App = ({ Component, pageProps }) => {
       <Head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=1, user-scalable=no"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
       </Head>
       {!DISABLE_LOADING && (
