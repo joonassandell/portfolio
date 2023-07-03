790c6fbb53af2b854554cbdeb45ed12cb258e4c3
Joonas Sandell <me@joonassandell.com>
2023-07-03 23:48:15 +0300

    Footer: simplify styles

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -8,18 +8,17 @@ export const Footer = () => {
     <footer className="Footer wrap">
       <div className="Footer-inner">
         <div className="Footer-main">
-          <ul className="Footer-links Footer-links--mobile">
-            {someLinks.map(link => {
+          <ul className="Footer-links">
+            {someLinks.map(item => {
               return (
-                <li className="Footer-links-item" key={link.id}>
-                  <LinkRoll href={link.url}>{link.title}</LinkRoll>
+                <li key={item.id}>
+                  <LinkRoll href={item.url}>{item.title}</LinkRoll>
                 </li>
               );
             })}
           </ul>
           <p className="Footer-copyright">
-            &copy; {new Date().getFullYear()}
-            <br />
+            &copy; {new Date().getFullYear()} <br />
             Joonas Sandell
           </p>
         </div>
