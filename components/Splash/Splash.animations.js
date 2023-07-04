3ca2075777692a8a2bbe82c994666ef4674fc47e
Joonas Sandell <me@joonassandell.com>
2023-07-04 06:24:09 +0300

    Splash: Speed up exit + cleanup

--- components/Splash/Splash.animations.js
+++ components/Splash/Splash.animations.js
@@ -1,17 +1,11 @@
 import { TRANS_PRIMARY, TRANS_SECONDARY } from '@/lib/config';
 
-/**
- * 1. This is here to wait the CSS animation to end
- */
 export const splashVariants = {
   exit: {
     clipPath: 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)',
     transition: {
       ...TRANS_SECONDARY,
-      delay: 1.6,
-      // delay: 0.4,
-      // delayChildren: TRANS_PRIMARY.duration,  // [1]
-      // when: 'afterChildren',
+      delay: 0.85,
     },
   },
   initial: {
@@ -23,7 +17,7 @@ export const textVariants = {
   exit: {
     transition: {
       ...TRANS_PRIMARY,
-      delay: 1.3, // [1]
+      delay: 0.75,
     },
     y: '-100%',
   },
