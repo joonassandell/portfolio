a866ff221d0e8d258ffd246da3373a1e45281ae8
Joonas Sandell <me@joonassandell.com>
2023-07-04 07:30:23 +0300

    Splash: Simplify and apply TextReveal matching animation

--- components/Splash/Splash.jsx
+++ components/Splash/Splash.jsx
@@ -1,5 +1,7 @@
 import { AnimatePresence, m } from 'framer-motion';
 import { splashVariants, textVariants } from './Splash.animations';
+import { Text } from '@/components/Text';
+import { Heading } from '@/components/Heading';
 
 export const Splash = ({ loading, setLoadingEnd }) => {
   return (
@@ -13,19 +15,17 @@ export const Splash = ({ loading, setLoadingEnd }) => {
           onAnimationComplete={() => setLoadingEnd(true)}
           variants={splashVariants}
         >
-          <div className="wrap">
-            <h1 className="Splash-content">
-              <div className="Splash-line Splash-line--subHeading">
-                <m.span className="Splash-text" variants={textVariants}>
-                  Portfolio of
-                </m.span>
-              </div>
-              <div className="Splash-line Splash-line--heading">
-                <m.span className="Splash-text" variants={textVariants}>
-                  Joonas Sandell
-                </m.span>
-              </div>
-            </h1>
+          <div className="Splash-main">
+            <Text className="Splash-row" size="xLarge">
+              <m.div className="Splash-text" variants={textVariants}>
+                Portfolio of
+              </m.div>
+            </Text>
+            <Heading className="Splash-row" size="h3" tag="div">
+              <m.div className="Splash-text" variants={textVariants}>
+                Joonas Sandell
+              </m.div>
+            </Heading>
           </div>
         </m.div>
       )}
