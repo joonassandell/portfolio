3d0bed69a37a55fe9c5ff7f89d9556b611728ea4
Joonas Sandell <me@joonassandell.com>
2024-05-03 04:08:40 +0300

    Convert useScrollTo to ts

--- components/LocomotiveScroll/useScrollTo.ts
+++ components/LocomotiveScroll/useScrollTo.ts
@@ -2,19 +2,25 @@ import { useLocomotiveScroll } from './';
 import { EASE, SCROLL_TO_DURATION } from '@/lib/config';
 
 export const useScrollTo = (
-  { scrollLock, callbackDelay } = {
+  {
+    scrollLock,
+    callbackDelay,
+  }: {
+    scrollLock?: boolean;
+    callbackDelay?: number;
+  } = {
     scrollLock: false,
     callbackDelay: 0,
   },
 ) => {
   const { scroll } = useLocomotiveScroll();
 
-  const scrollTo = (el, callback) => {
+  const scrollTo = (el: HTMLElement | number, callback?: () => void) => {
     if (scroll) {
       if (scrollLock) scroll.stop();
       scroll.scrollTo(el, {
         duration: SCROLL_TO_DURATION,
-        easing: EASE,
+        easing: EASE as [number, number, number, number],
         callback: () => {
           if (callback) {
             setTimeout(() => {
