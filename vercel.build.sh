8643428b0b28c2de7ce6f4eb2bb74b23d4ecfdaf
Joonas Sandell <me@joonassandell.com>
2024-05-04 17:09:49 +0300

    Set the build script properly

--- vercel.build.sh
+++ vercel.build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "VERCEL_ENV: $VERCEL_ENV"
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_ENV" == "production" || "$VERCEL_ENV" == "development" ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "development" ]] ; then
   # Proceed with the build
   echo "✅ - Build can proceed"
   exit 1;
