40a2ee94326b657a56a4a2d91fdfeab9c32a8318
Joonas Sandell <me@joonassandell.com>
2024-05-10 16:19:41 +0300

    Milestones: Add Mediasignal and Biocode events

--- features/Milestones/data/feature-release.ts
+++ features/Milestones/data/feature-release.ts
@@ -5,8 +5,15 @@ export const MILESTONES_FEATURE_RELEASE = [
 
   {
     category: 'feature-release',
+    categoryName: 'Feature proposal',
+    date: '2024-04-20',
+    event: 'Proposed new Biocode application UI',
+    major: true,
+  },
+  {
+    category: 'feature-release',
     categoryName: 'Feature release',
-    date: '2023-12-10',
+    date: '2024-01-01',
     event: 'Shipped stable Biocode Design System (BDS)',
     url: 'https://twitter.com/joonassandell/status/1733608503040741621',
   },
@@ -16,6 +23,12 @@ export const MILESTONES_FEATURE_RELEASE = [
     date: '2023-08-17',
     event: "Shipped light mode for Biocode's website",
     url: 'https://twitter.com/joonassandell/status/1733608503040741621',
+  },
+  {
+    category: 'feature-release',
+    categoryName: 'Feature release',
+    date: '2022-10-04',
+    event: 'Shipped Biocode Carbon Footprint label',
   },
   {
     category: 'feature-release',
@@ -30,5 +43,46 @@ export const MILESTONES_FEATURE_RELEASE = [
     date: '2021-07-21',
     event: 'Redesigned Valio Carbo® calculator app',
     url: 'https://www.valio.fi/uutiset/valio-ottaa-kayttoon-maitotiloille-raataloidyn-ymparistolaskurin-tavoitteena-pienentaa-paastoja-30-lla-5-vuodessa/',
+  },
+
+  /* =======================================
+   * Mediasignal
+   * ======================================= */
+
+  {
+    category: 'feature-release',
+    categoryName: 'Feature release',
+    date: '2019-09-26',
+    event: 'Shipped enhanced Podium web app UI',
+  },
+  {
+    category: 'feature-release',
+    categoryName: 'Feature release',
+    date: '2019-01-09',
+    event: 'Re-engineered Dictum website faster',
+  },
+  {
+    category: 'feature-release',
+    categoryName: 'Feature release',
+    date: '2017-06-26',
+    event: 'Shipped enhanced Ekovilla website UI',
+  },
+  {
+    category: 'feature-release',
+    categoryName: 'Feature release',
+    date: '2016-01-19',
+    event: 'Shipped Mackays campaign for Finefoods website',
+  },
+  {
+    category: 'feature-release',
+    categoryName: 'Feature release',
+    date: '2015-09-12',
+    event: 'Shipped enhanced SOS-Lapsikylä website',
+  },
+  {
+    category: 'feature-release',
+    categoryName: 'Feature release',
+    date: '2014-03-16',
+    event: 'Shipped enhanced Kanto marketing page',
   },
 ];
--- features/Milestones/data/project.ts
+++ features/Milestones/data/project.ts
@@ -50,7 +50,14 @@ export const MILESTONES_PROJECT = [
     categoryName: 'Project launch',
     date: '2021-01-26',
     event: 'Launched Biocode website',
-    url: 'https://biocode.io/sustainable-food-production',
+    url: 'https://twitter.com/joonassandell/status/1757856709563785723',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2021-01-26',
+    event: 'Launched Biocode website',
+    url: 'https://twitter.com/joonassandell/status/1757856709563785723',
   },
 
   /* =======================================
@@ -67,9 +74,39 @@ export const MILESTONES_PROJECT = [
   {
     category: 'project',
     categoryName: 'Project launch',
+    date: '2020-08-31',
+    event: 'Launched Katepal website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2020-09-26',
+    event: 'Launched SOS-Lapsikylä website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2019-12-05',
+    event: 'Launched Ursa website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
     date: '2019-11-19',
     event: 'Launched Hukka Food Waste web app',
     url: '/more-work#hukkas',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2019-09-27',
+    event: 'Launched SEY website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2019-09-06',
+    event: 'Launched Medetter OmaKlinikka web app',
   },
   {
     category: 'project',
@@ -82,10 +119,76 @@ export const MILESTONES_PROJECT = [
   },
   {
     category: 'project',
+    categoryName: 'Project proposal',
+    date: '2019-03-15',
+    event: 'Proposed NCC Smart Choices concept',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project proposal',
+    date: '2019-01-09',
+    event: 'Proposed new SOS-Lapsikylä website UI',
+  },
+  {
+    category: 'project',
     categoryName: 'Project launch',
-    date: '2017-11-9',
+    date: '2018-10-05',
+    event: 'Launched HW-Company e-commerce websites',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project delivery',
+    date: '2018-09-18',
+    event: 'Delivered NCC Asphalt project',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2018-05-29',
+    event: 'Launched Katepal Warranty Certificate web app',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2018-03-27',
+    event: 'Launched Enervent website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2018-03-10',
+    event: 'Launched Dahl website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2018-02-16',
+    event: 'Launched Reilukauppa website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2018-02-12',
+    event: 'Launched Jetecon website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2017-11-09',
     event: 'Launched TAKK website',
     url: '/more-work#takk',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2017-10-23',
+    event: 'Launched Messukeskus Document Generator web app',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2017-06-31',
+    event: 'Launched Coxa Pro web service',
   },
   {
     category: 'project',
@@ -97,11 +200,47 @@ export const MILESTONES_PROJECT = [
   {
     category: 'project',
     categoryName: 'Project launch',
+    date: '2017-04-20',
+    event: 'Launched Coxa website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2017-02-15',
+    event: 'Launched Kariplast website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2017-11-08',
+    event: 'Launched Tietotaito Group website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project proposal',
+    date: '2016-12-12',
+    event: 'Proposed new Jatke website UI',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2016-10-25',
+    event: 'Launched Kia Business website for RDKL',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
     date: '2016-07-06',
     event: 'Launched Oras website',
     highlight: true,
     major: true,
     url: '/oras',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project proposal',
+    date: '2016-06-01',
+    event: 'Proposed Ratina native app UI',
   },
   {
     category: 'project',
@@ -122,15 +261,133 @@ export const MILESTONES_PROJECT = [
   {
     category: 'project',
     categoryName: 'Project launch',
+    date: '2016-02-10',
+    event: 'Launched Puhdaspalvelu website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2016-01-05',
+    event: 'Launched Jollas website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2015-12-04',
+    event: 'Launched Movere website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2015-11-25',
+    event: 'Launched Tampereen Steinerkoulu website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2015-10-09',
+    event: 'Launched Finnpark website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2015-08-20',
+    event: 'Launched Avecra website',
+  },
+  {
+    category: 'feature-release',
+    categoryName: 'Feature release',
+    date: '2015-05-13',
+    event: 'Launched Finnpark Parking Reservation portal',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
     date: '2015-05-04',
-    event: 'Launched Finnpark Parking Guidance app',
-    highlight: true,
-    major: true,
+    event: 'Launched Finnpark Parking Guidance web app',
     url: '/more-work#finnpark',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2015-03-15',
+    event: 'Launched Finnpark Parking Administration web app',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2015-03-09',
+    event: 'Launched Finnpark Campus Parking web app',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2015-01-15',
+    event: 'Launched Finnpark Parking Benefit web app',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2015-06-16',
+    event: 'Launched Dahl Extranet web app',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2014-12-12',
+    event: 'Launced Paunu website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2014-12-25',
+    event: 'Launched Hotelzon website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2014-12-23',
+    event: 'Launched Meiranova website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2014-12-04',
+    event: 'Launched Huhtahyvät website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2014-11-21',
+    event: 'Launched Finnpark Parking Pre-reservation web app',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2014-10-30',
+    event: 'Launched Kesla website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2014-10-17',
+    event: 'Launched Valli website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2014-06-08',
+    event: 'Launched Tampereen vuokra-asunnot website',
+  },
+  {
+    category: 'project',
+    categoryName: 'Project launch',
+    date: '2014-04-07',
+    event: 'Launched Mediasignal website',
   },
 
   /* =======================================
-   * Company
+   * Personal company
    * ======================================= */
 
   {
@@ -148,8 +405,8 @@ export const MILESTONES_PROJECT = [
   {
     category: 'project',
     categoryName: 'Project launch',
-    date: '2016-12-29',
-    event: 'Launched Rebirth Starter Kit',
+    date: '2016-12-21',
+    event: 'Launched Rebirth UI',
     url: 'https://github.com/joonassandell/rebirth',
   },
 
