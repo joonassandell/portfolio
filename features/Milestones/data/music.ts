887bbfbdc327ff23251d120ec61c8a76d92270b9
Joonas Sandell <me@joonassandell.com>
2024-05-13 17:54:00 +0300

    Add various milestones, featured and releases

--- features/Milestones/data/music.ts
+++
@@ -1,1 +0,0 @@
-export const MILESTONES_MUSIC = [];
--- features/Milestones/data/various.ts
+++ features/Milestones/data/various.ts
@@ -1,1 +1,75 @@
-export const MILESTONES_VARIOUS = [];
+export const MILESTONES_VARIOUS = [
+  {
+    category: 'music-milestone',
+    categoryName: 'Hit a music milestone',
+    date: '2024',
+    event: 'Reached 700k listens on track Voyage on Spotify',
+    highlight: true,
+    url: 'https://open.spotify.com/artist/72bPxMUh9x57Nmvm2CqXal',
+  },
+  {
+    category: 'music-milestone',
+    categoryName: 'Hit a music milestone',
+    date: '2024',
+    event: 'Reached 175k listens on Orisha on Spotify',
+    url: 'https://open.spotify.com/track/2xQKZGHedY29bugRkY1TgS',
+  },
+  {
+    category: 'music-milestone',
+    categoryName: 'Hit a music milestone',
+    date: '2024',
+    event: 'Reached 80k listens on Ghamish on Spotify',
+    url: 'https://open.spotify.com/track/6HmYb783SQgK8GcxiolBsY',
+  },
+  {
+    category: 'music-milestone',
+    categoryName: 'Hit a music milestone',
+    date: '2024',
+    event: 'Hit 200k listens / downloads on Ananda Remix',
+    url: 'https://soundcloud.com/dancefloorromancer/free-download-stan-kolev-ananda-mode-apart-remix',
+  },
+  {
+    category: 'project-milestone',
+    categoryName: 'Hit a project milestone',
+    date: '2023-07-18',
+    event: 'Won Honorable mention with Joonas Sandell Portfolio',
+    url: 'https://www.awwwards.com/sites/joonas-sandell-portfolio',
+  },
+  {
+    category: 'music-milestone',
+    categoryName: 'Hit a music milestone',
+    date: '2020-10-27',
+    event:
+      'Reached #1 spot on Progressive House Chart and Top 20 Beatport main chart',
+    highlight: true,
+    url: 'https://www.instagram.com/p/CG2aCfpANNF',
+  },
+  {
+    category: 'project-milestone',
+    categoryName: 'Hit a project milestone',
+    date: '2013-04-17',
+    event: 'Won Honorable mention with my Pala taivasta website',
+    url: 'https://www.awwwards.com/sites/the-piece-of-heaven',
+  },
+  {
+    category: 'project-milestone',
+    categoryName: 'Hit a project milestone',
+    date: '2013-02-06',
+    event: 'Won Honorable mention with Moominvalley website',
+    url: 'https://www.awwwards.com/sites/moominvalley',
+  },
+  {
+    category: 'project-milestone',
+    categoryName: 'Hit a project milestone',
+    date: '2011-11-03',
+    event: 'Won Honorable mention with my Fullsize website',
+    url: 'https://www.awwwards.com/sites/fullsize',
+  },
+  {
+    category: 'music-milestone',
+    categoryName: 'Hit a music milestone',
+    date: '2010',
+    event: 'Reached 140k listens on No Matter on Youtube',
+    url: 'https://www.youtube.com/watch?v=zkzLjVa44U0',
+  },
+];
