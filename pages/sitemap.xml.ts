b001bf6fc9634956e19231b88b63b23c31c1b10c
Joonas Sandell <me@joonassandell.com>
2024-06-20 11:03:49 +0300

    Add sitemap.xml

---
+++ pages/sitemap.xml.ts
@@ -0,0 +1,47 @@
+import { APP_URL, SITEMAP } from '@/lib/config';
+import { type SitemapItem } from '@/types';
+import type { GetServerSideProps } from 'next';
+
+const generateSitemap = (sitemap: SitemapItem[]) => {
+  const uniqueURLs = (items: SitemapItem[]) => {
+    const seenUrls = new Set<string>();
+    return items.filter(({ url }) => {
+      if (seenUrls.has(url)) {
+        return false;
+      } else {
+        seenUrls.add(url);
+        return true;
+      }
+    });
+  };
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${uniqueURLs(
+    sitemap,
+  )
+    .map(({ url }) => {
+      return `
+  <url>
+    <loc>${`${APP_URL}${url}`}</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>always</changefreq>
+  </url>`;
+    })
+    .join('')}
+</urlset>`;
+};
+
+export default function Page() {}
+
+export const getServerSideProps = (async ({ res }) => {
+  const data = [...SITEMAP.common, ...SITEMAP.project];
+  const sitemap = generateSitemap(data);
+
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}) satisfies GetServerSideProps;
