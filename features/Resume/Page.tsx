c8c63e08b0f591fb81efc7d1edb0b16e1072c2c0
Joonas Sandell <me@joonassandell.com>
2024-06-25 00:05:38 +0300

    Resume: Add Stepper, tweak content

--- features/Resume/Page.tsx
+++ features/Resume/Page.tsx
@@ -1,11 +1,12 @@
 import { Button } from '@/components/Button';
-import { Download } from '@/components/Icon';
+import { Check, Download } from '@/components/Icon';
 import { Figure } from '@/components/Figure';
 import { getLink, getSitemap, stripUrl } from '@/lib/utils';
 import { Head } from '@/components/Head';
 import { Heading } from '@/components/Heading';
 import { Link } from '@/components/Link';
 import { type PageProps } from '@/types';
+import { Stepper, StepperItem, StepperStep } from '@/components/Stepper';
 import { Template, TemplateMain, TemplateSection } from '@/components/Template';
 import { Text } from '@/components/Text';
 import { useSetThemeColor } from '@/components/App';
@@ -59,163 +60,176 @@ export const ResumePage = ({ id, themeColor, title }: PageProps) => {
             <Heading className="mb:m" size="h4" tag="h2">
               Experience
             </Heading>
-            <Heading className="mb:2xs" size="h6" tag="h3">
-              Lead product designer, Front-end developer — Biocode
-            </Heading>
-            <Text className="mb" color="mute">
-              Nov 2020 —
-            </Text>
-            <Text className="mb:l">
-              <ul>
-                <li>
-                  Built the{' '}
-                  <Link href={getSitemap('biocode').url}>application</Link>{' '}
-                  front-end together with the team including various features
-                  such as real time carbon footprint calculation
-                </li>
-                <li>
-                  Designed the application and its features including prototypes
-                  and user flows with focus on detailed UX/UI, accessibility and
-                  ease-of-use
-                </li>
-                <li>
-                  Played an active role in enhancing overall direction of the
-                  product and suggesting user-centric features based on
-                  analytics
-                </li>
-                <li>
-                  Built and designed{' '}
-                  <Link href={getSitemap('biocode').url}>Biocode</Link>’s
-                  end-to-end website including dark/light modes, CMS and Hubspot
-                  integration among many other features
-                </li>
-                <li>
-                  Designed, built and authored Biocode Design System including
-                  brand assets such as logomarks, icons and product packaging
-                  stamps
-                </li>
-                <li>
-                  Designed and built headless{' '}
-                  <Link href="https://report.biocode.io">
-                    Biocode for reporting
-                  </Link>{' '}
-                  Next.js web service to market the impact of Biocode for the
-                  public audience
-                </li>
-                <li>
-                  Contributed and planned interviews with the marketing team for
-                  users in order to improve the UX and UI of the application
-                </li>
-                <li>
-                  Aided other engineers to ensure quality UI/UX and
-                  implementation
-                </li>
-              </ul>
-            </Text>
-            <Heading className="mb:2xs" size="h6" tag="h3">
-              UI/UX Designer, Front-end developer — Mediasignal
-            </Heading>
-            <Text className="mb" color="mute" size="m">
-              Sep 2014 — Nov 2020
-            </Text>
-            <Text className="mb:l">
-              <ul>
-                <li>
-                  Developed and designed applications for clients such as{' '}
-                  <Link
-                    href={`${getSitemap('more-work').url}/#academic-bookstore`}
-                  >
-                    Bonnier Books
-                  </Link>
-                  ,{' '}
-                  <Link href={`${getSitemap('more-work').url}/#hukka`}>
-                    Luke
-                  </Link>
-                  , Finefoods, Kesla, Dahl and{' '}
-                  <Link href={`${getSitemap('more-work').url}/#finnpark`}>
-                    Finnpark
-                  </Link>
-                </li>
-                <li>
-                  Developed and designed websites, e-commerce stores and web
-                  services for clients such as{' '}
-                  <Link
-                    href={`${getSitemap('more-work').url}/#academic-bookstore`}
-                  >
-                    Academic Bookstore
-                  </Link>
-                  , Kia, Caverion, Fair trade,{' '}
-                  <Link href={`${getSitemap('more-work').url}/#takk`}>
-                    TAKK
-                  </Link>
-                  , <Link href={getSitemap('oras').url}>Oras</Link> and{' '}
-                  <Link href={`${getSitemap('more-work').url}/#hankkija`}>
-                    Hankkija
-                  </Link>
-                </li>
-                <li>
-                  Arranged workshops and planned UX interviews to get valuable
-                  feedback from clients and end-users to build successful
-                  products
-                </li>
-                <li>
-                  Refreshed{' '}
-                  <Link href={getSitemap('mediasignal').url}>
-                    Mediasignal’s brand twice
-                  </Link>{' '}
-                  including logomark design, brand book crafting, business card
-                  design and website renewal
-                </li>
-                <li>
-                  Designed and built internal products such as{' '}
-                  <Link href={`${getSitemap('more-work').url}/#rubik`}>
-                    Rubik
-                  </Link>{' '}
-                  (PIM), Avainia and ImageBank that were sold to variety of
-                  customers
-                </li>
-                <li>
-                  Crafted and documented internal tools to quickly kick-start
-                  projects
-                </li>
-                <li>
-                  Managed to acquire new clients based on past experience,
-                  outside connections and word-to-mouth reputation
-                </li>
-                <li>
-                  Led and aided groups of developers and communicated with the
-                  customers about project progress’
-                </li>
-              </ul>
-            </Text>
-            <Heading className="mb:2xs" size="h6" tag="h3">
-              UI/UX Designer, Front-end developer — City of Tampere
-            </Heading>
-            <Text className="mb" color="mute">
-              Apr 2007 — Sep 2014
-            </Text>
-            <Text className="mb:m">
-              <ul>
-                <li>
-                  Built and curated valuable Exhibition Guide mobile app to help
-                  visitors gain an enjoyable and insightful museum experiences
-                </li>
-                <li>
-                  Built Museum Guide screen which featured maps, restaurant
-                  menus and other museum information integrated to the website
-                  CMS
-                </li>
-                <li>
-                  Built Twitter Social Wall screen to engage museum visitors
-                  sharing their museum experiences
-                </li>
-                <li>
-                  Developed and designed websites and posters for Museum Centre
-                  Vapriikki, Moominvalley, Tampere Music Festivals and other
-                  city sectors
-                </li>
-              </ul>
-            </Text>
+            <Stepper>
+              <StepperItem>
+                <StepperStep beacon />
+                <Heading className="mb:2xs" size="h6" tag="h3">
+                  Lead product designer, Front-end developer — Biocode
+                </Heading>
+                <Text className="mb" color="mute">
+                  Nov 2020 — Present
+                </Text>
+                <Text className="mb:l">
+                  <ul className="pl">
+                    <li>
+                      Built the{' '}
+                      <Link href={getSitemap('biocode').url}>application</Link>{' '}
+                      front-end together with the team including various
+                      features such as real time carbon footprint calculation
+                    </li>
+                    <li>
+                      Designed the application and its features including
+                      prototypes and user flows with focus on detailed UX/UI,
+                      accessibility and ease-of-use
+                    </li>
+                    <li>
+                      Played an active role in enhancing overall direction of
+                      the product and suggesting user-centric features based on
+                      analytics
+                    </li>
+                    <li>
+                      Built and designed{' '}
+                      <Link href={getSitemap('biocode').url}>Biocode</Link>’s
+                      end-to-end website including dark/light modes, CMS and
+                      Hubspot integration among many other features
+                    </li>
+                    <li>
+                      Designed, built and authored Biocode Design System
+                      including brand assets such as logomarks, icons and
+                      product packaging stamps
+                    </li>
+                    <li>
+                      Designed and built headless{' '}
+                      <Link href="https://report.biocode.io">
+                        Biocode for reporting
+                      </Link>{' '}
+                      Next.js web service to market the impact of Biocode for
+                      the public audience
+                    </li>
+                    <li>
+                      Contributed and planned interviews with the marketing team
+                      for users in order to improve the UX and UI of the
+                      application
+                    </li>
+                    <li>
+                      Aided other engineers to ensure quality UI/UX and
+                      implementation
+                    </li>
+                  </ul>
+                </Text>
+              </StepperItem>
+              <StepperItem>
+                <StepperStep icon={<Check />} />
+                <Heading className="mb:2xs" size="h6" tag="h3">
+                  UI/UX Designer, Front-end developer — Mediasignal
+                </Heading>
+                <Text className="mb" color="mute" size="m">
+                  Sep 2014 — Nov 2020
+                </Text>
+                <Text className="mb:l">
+                  <ul className="pl">
+                    <li>
+                      Developed and designed applications for clients such as{' '}
+                      <Link
+                        href={`${getSitemap('more-work').url}/#academic-bookstore`}
+                      >
+                        Bonnier Books
+                      </Link>
+                      ,{' '}
+                      <Link href={`${getSitemap('more-work').url}/#hukka`}>
+                        Luke
+                      </Link>
+                      , Finefoods, Kesla, Dahl and{' '}
+                      <Link href={`${getSitemap('more-work').url}/#finnpark`}>
+                        Finnpark
+                      </Link>
+                    </li>
+                    <li>
+                      Developed and designed websites, e-commerce stores and web
+                      services for clients such as{' '}
+                      <Link
+                        href={`${getSitemap('more-work').url}/#academic-bookstore`}
+                      >
+                        Academic Bookstore
+                      </Link>
+                      , Kia, Caverion, Fair trade,{' '}
+                      <Link href={`${getSitemap('more-work').url}/#takk`}>
+                        TAKK
+                      </Link>
+                      , <Link href={getSitemap('oras').url}>Oras</Link> and{' '}
+                      <Link href={`${getSitemap('more-work').url}/#hankkija`}>
+                        Hankkija
+                      </Link>
+                    </li>
+                    <li>
+                      Arranged workshops and planned UX interviews to get
+                      valuable feedback from clients and end-users to build
+                      successful products
+                    </li>
+                    <li>
+                      Refreshed{' '}
+                      <Link href={getSitemap('mediasignal').url}>
+                        Mediasignal’s brand twice
+                      </Link>{' '}
+                      including logomark design, brand book crafting, business
+                      card design and website renewal
+                    </li>
+                    <li>
+                      Designed and built internal products such as{' '}
+                      <Link href={`${getSitemap('more-work').url}/#rubik`}>
+                        Rubik
+                      </Link>{' '}
+                      (PIM), Avainia and ImageBank that were sold to variety of
+                      customers
+                    </li>
+                    <li>
+                      Crafted and documented internal tools to quickly
+                      kick-start projects
+                    </li>
+                    <li>
+                      Managed to acquire new clients based on past experience,
+                      outside connections and word-to-mouth reputation
+                    </li>
+                    <li>
+                      Led and aided groups of developers and communicated with
+                      the customers about project progress’
+                    </li>
+                  </ul>
+                </Text>
+              </StepperItem>
+              <StepperItem>
+                <StepperStep icon={<Check />} />
+                <Heading className="mb:2xs" size="h6" tag="h3">
+                  UI/UX Designer, Front-end developer — City of Tampere
+                </Heading>
+                <Text className="mb" color="mute">
+                  Apr 2007 — Sep 2014
+                </Text>
+                <Text className="mb:m">
+                  <ul className="pl">
+                    <li>
+                      Built and curated valuable Exhibition Guide mobile app to
+                      help visitors gain an enjoyable and insightful museum
+                      experiences
+                    </li>
+                    <li>
+                      Built Museum Guide screen which featured maps, restaurant
+                      menus and other museum information integrated to the
+                      website CMS
+                    </li>
+                    <li>
+                      Built Twitter Social Wall screen to engage museum visitors
+                      sharing their museum experiences
+                    </li>
+                    <li>
+                      Developed and designed websites and posters for Museum
+                      Centre Vapriikki, Moominvalley, Tampere Music Festivals
+                      and other city sectors
+                    </li>
+                  </ul>
+                </Text>
+              </StepperItem>
+            </Stepper>
           </div>
           <aside className="Template-aside grid-col grid-col:3@m">
             <Figure
@@ -241,11 +255,6 @@ export const ResumePage = ({ id, themeColor, title }: PageProps) => {
                 </Link>
               </li>
               <li>Helsinki, Finland</li>
-              <li>
-                <Link href={getSitemap('contact', 'common').url} truncate>
-                  Contact me
-                </Link>
-              </li>
             </Text>
             <Text className="mb:l visible@m" size="s" tag="ul">
               <li>
