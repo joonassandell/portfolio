d66ff22f8efb3aef4293400552c2edd4f28491a6
Joonas Sandell <me@joonassandell.com>
2024-07-04 12:15:37 +0300

    Remove old sitemap material

--- types/common.ts
+++ types/common.ts
@@ -1,39 +1,4 @@
 /* =======================================
- * Sitemap, links
- * ======================================= */
-
-export interface SitemapItem {
-  hidden?: boolean;
-  id: string;
-  imagesPath?: string;
-  navTitle: string;
-  themeColor?: string;
-  title: string;
-  url: URL['href'];
-}
-
-export interface SitemapItemProject extends SitemapItem {
-  color: string;
-  year: string | number;
-}
-
-export interface Sitemap {
-  common: SitemapItem[];
-  project: SitemapItemProject[];
-}
-
-export interface Link {
-  id: string;
-  title: string;
-  url: URL['href'];
-}
-
-export interface Links {
-  common: Link[];
-  social: Link[];
-}
-
-/* =======================================
  * Page
  * ======================================= */
 
