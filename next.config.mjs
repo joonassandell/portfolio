605a643592dc655a234d1829dce8e154ec76cf25
Joonas Sandell <me@joonassandell.com>
2024-07-26 17:36:42 +0300

    Add sun/moon. Don't fill if none. Disable svgo.
    For some reason options.svgoConfig.plugins[{ name: 'preset-default', params.overriders: { ... } }] didn't work, so disable the whole svgo.

--- next.config.mjs
+++ next.config.mjs
@@ -38,7 +38,14 @@ const config = {
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ['@svgr/webpack'],
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgo: false,
+          },
+        },
+      ],
     });
     return config;
   },
