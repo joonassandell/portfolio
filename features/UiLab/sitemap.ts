38a5ee9fc36a5244efef0893a0d308839b6b6863
Joonas Sandell <me@joonassandell.com>
2024-08-02 15:58:31 +0300

    UiLab: Update description

--- features/UiLab/sitemap.ts
+++ features/UiLab/sitemap.ts
@@ -3,7 +3,8 @@ import { type SitemapItem } from '@/lib/sitemap';
 const sitemap: SitemapItem<'uiLab'> = {
   id: 'uiLab',
   meta: {
-    description: 'User interface laboratory by Joonas Sandell',
+    description:
+      'User interface laboratory is a showcase of various UI component experiments and interactive design concepts.',
     ogImage: '/ui-lab/og.jpg',
     themeColor: '#09090b',
     title: 'UI Laboratory',
