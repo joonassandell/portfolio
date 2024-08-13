f3b72b5c897d69ff96d1823749bcd10dbdaa4765
Joonas Sandell <me@joonassandell.com>
2024-08-13 16:22:55 +0300

    About: Convert to useParallax

--- features/About/Top.tsx
+++ features/About/Top.tsx
@@ -1,54 +1,56 @@
 import { Figure } from '@/components/Figure';
 import { Heading } from '@/components/Heading';
-import { MQ, SCROLL_SPEED } from '@/lib/config';
+import { MQ } from '@/lib/config';
 import { TextReveal } from '@/components/TextReveal';
+import { useParallax } from '@/lib/useParallax';
 import lineCube from '@/public/images/line-cube.png';
 import profile from '@/public/images/joonassandell-profile.png';
 
-export const AboutTop = () => (
-  <>
-    <div className="Template-heading wrap pr:0">
-      <Heading
-        aria-hidden
-        className="white-space:nowrap mb:0"
-        data-scroll
-        data-scroll-direction="horizontal"
-        data-scroll-position="left"
-        data-scroll-speed={SCROLL_SPEED}
-        size="display"
-        tag="div"
-      >
-        <TextReveal text={["Hello ✳︎ Moro ✳︎ Hi ✳︎ What's up?"]} />
-      </Heading>
-    </div>
-    <div className="Template-figure grid">
-      <div className="grid-col grid-col:10 -start:3 grid-col:6@m -start:7@m">
-        <Figure
-          alt="Joonas Sandell profile"
-          borderRadius={false}
-          priority
-          quality={90}
-          scroll="mask"
-          scrollImageSpeed={-0.26}
-          scrollPosition="top"
-          sizes={`${MQ.s} 50vw, 80vw`}
-          transition="clip"
-          {...profile}
-        />
-        <Figure
-          alt="Line cube"
-          aria-hidden="true"
-          borderRadius={false}
-          className="Template-cube"
-          placeholder={false}
-          priority
-          scroll
-          scrollPosition="top"
-          scrollSpeed={0.2}
-          sizes="33vw"
-          {...lineCube}
-        />
+export const AboutTop = () => {
+  const { ref, value: x } = useParallax({ offset: 'start-start' });
+
+  return (
+    <>
+      <div className="Template-heading wrap pr:0">
+        <Heading
+          aria-hidden
+          className="white-space:nowrap mb:0"
+          size="display"
+          style={{ x }}
+          tag="div"
+        >
+          <TextReveal text={["Hello ✳︎ Moro ✳︎ Hi ✳︎ What's up?"]} />
+        </Heading>
       </div>
-    </div>
-  </>
-);
+      <div className="Template-figure grid" ref={ref}>
+        <div className="grid-col grid-col:10 -start:3 grid-col:6@m -start:7@m">
+          <Figure
+            alt="Joonas Sandell profile"
+            borderRadius={false}
+            priority
+            quality={90}
+            scroll="mask"
+            scrollImageSpeed="fastest"
+            scrollOffset="start-start"
+            sizes={`${MQ.s} 50vw, 80vw`}
+            transition="clip"
+            {...profile}
+          />
+          <Figure
+            alt="Line cube"
+            aria-hidden="true"
+            borderRadius={false}
+            className="Template-cube"
+            placeholder={false}
+            priority
+            scroll
+            scrollOffset="start-start"
+            scrollSpeed="fast"
+            sizes="33vw"
+            {...lineCube}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
