27595a5cce886449b244642163f7070bb2103011
Joonas Sandell <me@joonassandell.com>
2024-08-27 00:53:22 +0300

    Heading: Add maxWidth and built-in animate

--- components/Heading/Heading.tsx
+++ components/Heading/Heading.tsx
@@ -1,23 +1,41 @@
-import { forwardRef } from 'react';
+import { forwardRef, type RefObject, useRef } from 'react';
 import { type HeadingProps } from './';
 import { m } from 'framer-motion';
+import { TEXT_VARIANTS } from '@/lib/config';
+import { useInView } from '@/lib/useInView';
 import c from 'clsx';
 
 export const Heading = forwardRef<HTMLHeadingElement, HeadingProps>(
-  ({ children, className, size = 'h2', tag, ...props }, forwardedRef) => {
+  (
+    { animate, children, className, maxWidth, size = 'h2', tag, ...props },
+    forwardedRef,
+  ) => {
     const display = size === 'display';
-    const Tag = tag ? m[tag] : display ? m.h2 : m[size];
     const classes = c(
       'Heading',
       {
         [`${size}`]: size && size != tag && !display,
+        '-maxWidth': maxWidth,
         'Heading--display': display,
       },
       className,
     );
+    const createdRef = useRef(null);
+    const ref = (forwardedRef as RefObject<HTMLHeadingElement>) ?? createdRef;
+    const inView = useInView(ref);
+    const Tag = tag ? m[tag] : display ? m.h2 : m[size];
 
     return (
-      <Tag className={classes} ref={forwardedRef} {...props}>
+      <Tag
+        className={classes}
+        ref={ref}
+        {...(animate && {
+          animate: inView ? 'animate' : '',
+          initial: 'initial',
+          variants: TEXT_VARIANTS,
+        })}
+        {...props}
+      >
         {children}
       </Tag>
     );
--- components/Heading/Heading.types.ts
+++ components/Heading/Heading.types.ts
@@ -2,6 +2,8 @@ import { type HTMLMotionProps } from 'framer-motion';
 import { type TagGeneric, type TagHeading } from '@/types';
 
 export interface HeadingProps extends HTMLMotionProps<TagHeading | TagGeneric> {
+  animate?: boolean;
+  maxWidth?: boolean;
   size?: TagHeading | 'display';
   tag?: TagHeading | TagGeneric;
 }
