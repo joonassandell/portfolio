da20e9d7f6f79316cda2655c33725c8607e76802
Joonas Sandell <me@joonassandell.com>
2024-11-28 18:52:42 +0200

    Revert to next.js 14
    Totally random layout shifts in lighthouse every now and then with next 15. This didn't happen ever in v14, so revert.

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
-import bundleAnalyzer from '@next/bundle-analyzer'
 import path from 'path'
-import type { NextConfig } from 'next'
+import bundleAnalyzer from '@next/bundle-analyzer'
+import { fileURLToPath } from 'url'
 
-const { ANALYZE, IGNORE_ERRORS, VERCEL_ENV } = process.env
+const { ANALYZE, VERCEL_ENV, IGNORE_ERRORS } = process.env
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: ANALYZE === 'true',
 })
 
-const config: NextConfig = {
+/** @type {import('next').NextConfig} */
+const config = {
   env: {
     NEXT_PUBLIC_BUILD_DATE: new Date().toISOString(),
   },
@@ -17,14 +20,6 @@ const config: NextConfig = {
   },
   experimental: {
     optimizeCss: true,
-    turbo: {
-      rules: {
-        '*.svg': {
-          as: '*.js',
-          loaders: ['@svgr/webpack'],
-        },
-      },
-    },
   },
   ...(VERCEL_ENV === 'production' && {
     compiler: {
@@ -33,31 +28,8 @@ const config: NextConfig = {
       },
     },
   }),
-  async headers() {
-    return [
-      {
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=31536000, must-revalidate',
-          },
-        ],
-        locale: false,
-        source: '/(.*).(jpe?g|png|ico|webp|svg|xml|mp4|woff2?)',
-      },
-    ]
-  },
   images: {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 480],
-  },
-  async redirects() {
-    return [
-      {
-        destination: '/Joonas-Sandell-CV.pdf',
-        permanent: false,
-        source: '/cv',
-      },
-    ]
   },
   sassOptions: {
     includePaths: [path.join(__dirname), `${path.join(__dirname)}/stylesheets`],
@@ -68,11 +40,33 @@ const config: NextConfig = {
   },
   webpack(config) {
     config.module.rules.push({
-      test: /\.svg$/i,
+      test: /\.svg$/,
       use: ['@svgr/webpack'],
     })
-
     return config
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*).(jpe?g|png|ico|webp|svg|xml|mp4|woff2?)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, must-revalidate',
+          },
+        ],
+      },
+    ]
+  },
+  async redirects() {
+    return [
+      {
+        source: '/cv',
+        destination: '/Joonas-Sandell-CV.pdf',
+        permanent: false,
+      },
+    ]
   },
 }
 
