831d3747bb9abf5edfaac596a077fe2b80645d9f
Joonas Sandell <me@joonassandell.com>
2024-12-05 16:59:31 +0200

    Splash: Remove text animation, exit faster

--- components/Splash/Splash.config.ts
+++ components/Splash/Splash.config.ts
@@ -1,25 +1,17 @@
-import { TRANS_PRIMARY, TRANS_TEMPLATE } from '@/lib/config'
+import { TRANS_TEMPLATE } from '@/lib/config'
 import { type Variants } from 'motion/react'
 
 export const SPLASH_VARIANTS: Readonly<Variants> = {
   exit: {
     clipPath: 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)',
+    opacity: 1, // Just for detecting the exit animation with CSS
     transition: {
       ...TRANS_TEMPLATE,
-      delay: 0.85,
+      delay: 0.7,
+      duration: 0.8,
     },
   },
   initial: {
     clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
-  },
-}
-
-export const TEXT_VARIANTS: Readonly<Variants> = {
-  exit: {
-    transition: {
-      ...TRANS_PRIMARY,
-      delay: 0.75,
-    },
-    y: '-100%',
   },
 }
--- components/Splash/Splash.tsx
+++ components/Splash/Splash.tsx
@@ -1,6 +1,6 @@
 import { AnimatePresence, m } from 'motion/react'
 import { Heading } from '@/components/Heading'
-import { SPLASH_VARIANTS, type SplashProps, TEXT_VARIANTS } from './'
+import { SPLASH_VARIANTS, type SplashProps } from './'
 
 export const Splash = ({ loading, onAnimationComplete }: SplashProps) => {
   return (
@@ -16,22 +16,10 @@ export const Splash = ({ loading, onAnimationComplete }: SplashProps) => {
         >
           <div className="Splash-main">
             <Heading className="Splash-row mb:2xs" size="h6" tag="div">
-              <m.div
-                className="Splash-text"
-                exit="exit"
-                variants={TEXT_VARIANTS}
-              >
-                Portfolio of
-              </m.div>
+              <div className="Splash-text">Portfolio of</div>
             </Heading>
             <Heading className="Splash-row mb:m" size="h3" tag="div">
-              <m.div
-                className="Splash-text"
-                exit="exit"
-                variants={TEXT_VARIANTS}
-              >
-                Joonas Sandell
-              </m.div>
+              <div className="Splash-text">Joonas Sandell</div>
             </Heading>
             <div className="Splash-loader" />
           </div>
