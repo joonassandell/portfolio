a6c35d3ba9a9c236a10f6c56eac768e670ecc746
Joonas Sandell <me@joonassandell.com>
2024-12-12 16:44:23 +0200

    Home: Add slight mouse move teaser for pfp

--- features/Home/HeroPfp.tsx
+++ features/Home/HeroPfp.tsx
@@ -7,11 +7,18 @@ import {
   useSpring,
   useTransform,
 } from 'motion/react'
-import { MQ, TRANS_DRAG, TRANS_SPRING, TRANS_TAP } from '@/lib/config'
+import {
+  MQ,
+  TRANS_DRAG,
+  TRANS_MOVE,
+  TRANS_SPRING,
+  TRANS_TAP,
+} from '@/lib/config'
 import { useApp } from '@/components/App'
 import { useState } from 'react'
 import pfp from '@/public/common/joonassandell/joonassandell-pfp-with-avatar.jpg'
 import PfpAvatar from '@/public/common/joonassandell/joonassandell-pfp-avatar.svg'
+import useMeasure from 'react-use-measure'
 
 export const HomeHeroPfp = ({ drag, setActive, setDrag }: HomeHeroPfpProps) => {
   const {
@@ -23,71 +30,87 @@ export const HomeHeroPfp = ({ drag, setActive, setDrag }: HomeHeroPfpProps) => {
   const dragControls = useDragControls()
   const dragX = useMotionValue(0)
   const dragY = useMotionValue(0)
-  const dragDistance = useTransform(
-    [dragX, dragY],
-    ([x, y]: number[]) => Math.sqrt(x * x + y * y), // Distance from the center
+  const dragDistanceCenter = useTransform([dragX, dragY], ([x, y]: number[]) =>
+    Math.sqrt(x * x + y * y),
   )
-  const figureTrans = useTransform(dragDistance, [-40, 0, 40], [1, 0.5, 1])
-  const figureVal = useSpring(figureTrans, TRANS_SPRING.fast)
+  const figureVal = useSpring(
+    useTransform(dragDistanceCenter, [-40, 0, 40], [1, 0.5, 1]),
+    TRANS_SPRING.fast,
+  )
+
+  const [mouseRef, bounds] = useMeasure({ scroll: false })
+  const mouseX = useMotionValue(0)
+  const mouseY = useMotionValue(0)
+  const moveX = useSpring(
+    useTransform(mouseX, x => x / 10),
+    TRANS_MOVE.fast,
+  )
+  const moveY = useSpring(
+    useTransform(mouseY, y => y / 10),
+    TRANS_MOVE.fast,
+  )
 
   return (
-    <div
-      className="Template-hero-pfp mb:m mb:ml@l"
-      style={{
-        cursor: tap || drag ? 'grabbing' : 'grab',
-      }}
-    >
+    <div className="Template-hero-pfp mb:m mb:ml@l">
       <m.div
-        className="Template-hero-pfp-figure"
-        style={{ opacity: figureVal, scale: figureVal }}
-        transition={TRANS_TAP}
+        className="Template-hero-pfp-main"
+        style={{
+          x: moveX,
+          y: moveY,
+        }}
       >
-        <Figure
-          alt="Joonas Sandell profile picture"
-          animate={false}
-          placeholder={false}
-          priority
-          quality={90}
-          sizes={`${MQ['xl']} 10vw, 7rem`}
-          {...pfp}
-        />
-      </m.div>
-      <m.div
-        animate={{
-          scale: tap ? 0.9 : 1,
-        }}
-        className="Template-hero-pfp-avatar"
-        drag
-        dragConstraints={{
-          bottom: 80,
-          left: -80,
-          right: 80,
-          top: -80,
-        }}
-        dragControls={dragControls}
-        dragElastic={0.3}
-        dragSnapToOrigin
-        dragTransition={TRANS_DRAG}
-        onDrag={(_, info) => {
-          dragX.set(info.offset.x)
-          dragY.set(info.offset.y)
-        }}
-        onDragEnd={() => {
-          dragX.set(0)
-          dragY.set(0)
-          setDrag(false)
-          if (!hover) setActive(false)
-        }}
-        onDragStart={() => {
-          setDrag(true)
-          setTap(false)
-        }}
-        transition={TRANS_TAP}
-      >
-        <PfpAvatar aria-hidden />
+        <m.div
+          className="Template-hero-pfp-figure"
+          style={{ opacity: figureVal, scale: figureVal }}
+          transition={TRANS_TAP}
+        >
+          <Figure
+            alt="Joonas Sandell profile picture"
+            animate={false}
+            placeholder={false}
+            priority
+            quality={90}
+            sizes={`${MQ['xl']} 10vw, 7rem`}
+            {...pfp}
+          />
+        </m.div>
+        <m.div
+          animate={{
+            scale: tap ? 0.9 : 1,
+          }}
+          className="Template-hero-pfp-avatar"
+          drag
+          dragConstraints={{
+            bottom: 80,
+            left: -80,
+            right: 80,
+            top: -80,
+          }}
+          dragControls={dragControls}
+          dragElastic={0.3}
+          dragSnapToOrigin
+          dragTransition={TRANS_DRAG}
+          onDrag={(_, info) => {
+            dragX.set(info.offset.x)
+            dragY.set(info.offset.y)
+          }}
+          onDragEnd={() => {
+            dragX.set(0)
+            dragY.set(0)
+            setDrag(false)
+            if (!hover) setActive(false)
+          }}
+          onDragStart={() => {
+            setDrag(true)
+            setTap(false)
+          }}
+          transition={TRANS_TAP}
+        >
+          <PfpAvatar aria-hidden />
+        </m.div>
       </m.div>
       <div
-        className="Template-hero-pfp-boundary"
+        className="Template-hero-pfp-dragBound"
         onPointerDown={e => {
           dragControls.start(e)
           setTap(true)
@@ -101,6 +124,24 @@ export const HomeHeroPfp = ({ drag, setActive, setDrag }: HomeHeroPfpProps) => {
           if (!hasTouch) setHover(false)
         }}
         onPointerUp={() => setTap(false)}
+      />
+      <div
+        className="Template-hero-pfp-mouseBound"
+        onPointerLeave={() => {
+          if (hasTouch) return
+          mouseX.set(0)
+          mouseY.set(0)
+        }}
+        onPointerMove={e => {
+          if (drag || tap || hasTouch) return
+
+          // Horizontal position relative to the center of the element
+          mouseX.set(e.clientX - bounds.x - bounds.width / 2)
+
+          // Vertical position relative to the center of the element
+          mouseY.set(e.clientY - bounds.y - bounds.height / 2)
+        }}
+        ref={mouseRef}
       />
     </div>
   )
